1ece292df93d27f196374feb5784e93d7cec1dae
Filip Koniuszewski <filip7446@gmail.com>
2022-06-29 16:08:38 +0200

    implemented modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useRef, useState} from "react";
+import RegisterModal from "./Components/RegisterModal";
+import Button from "./Components/Button";
+import "./Styles/App.scss"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    
+    const modalRef = useRef(null)
+
+    useEffect(() => {
+        if (isModalOpen) {
+            modalRef.current.classList.add('register-modal-active')
+        }
+        else {
+            modalRef.current.classList.remove('register-modal-active')
+        }
+
+    }, [isModalOpen])
+
+    useEffect(() => {
+        const checkIfClickedOutside = (e) => {
+            if (isModalOpen && modalRef.current && !modalRef.current.contains(e.target)) {
+                setIsModalOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", checkIfClickedOutside);
+        return () => {
+            document.removeEventListener("mousedown", checkIfClickedOutside);
+        };
+    }, [isModalOpen]);
+    
+   
+    
+    return (
+        <div className="App">
+            <div className={`wrapper ${isModalOpen && 'bg-modal-open'}`}>
+                {isModalOpen ?
+                    <Button
+                        clickEvent={() => setIsModalOpen(true)}
+                        content="You clicked me!"
+                        className="open-modal-button opened"/>
+                    :
+                    <Button
+                        clickEvent={() => setIsModalOpen(true)}
+                        content="Click me!"
+                        className="open-modal-button"/> }
+            </div>
+            <RegisterModal
+                modalRef={modalRef}
+                isModalOpen={isModalOpen}
+                clickEvent={() => setIsModalOpen(false)}
+            />
+        </div>
   );
 }
 
---
+++ src/Components/Button.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+export default function Button({clickEvent, content, className}) {
+    return (
+        <button onClick={clickEvent} className={className}>
+            {content}
+        </button>
+    );
+}
